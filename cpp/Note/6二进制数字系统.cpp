8a7e43e0050a4a7c414abd8793d023944458a7c5
W23x <www.zh0408@gmail.com>
2026-01-25 23:07:03 +0800

    1

--- cpp/Note/6二进制数字系统.cpp
+++ cpp/Note/6二进制数字系统.cpp
@@ -12,27 +12,37 @@ int main(){
     //基数base：二进制、八进制、十进制、十六进制
     //位权weighit：每一位数字所代表的值
 
-    //十进制转二进制
-    long long n,b;
-    cout<<"请输入一个十进制整数：";
-    cin >> n;
-    bool k = false;
-    for (int i=100;i>0;i--){
-        b=n;
-         for (int j = 1; j < i; j++) {
-            b = b / 2;
-        }
-        if (b % 2 == 1) {
-            k = true; 
-        }
-        if (k == true) {
-            cout << b % 2; 
-        }
+//十进制转二进制
+    // long long n,b;
+    // cout<<"请输入一个十进制整数：";
+    // cin >> n;
+    // bool k = false;
+    // for (int i=100;i>0;i--){
+    //     b=n;
+    //      for (int j = 1; j < i; j++) {
+    //         b = b / 2;
+    //     }
+    //     if (b % 2 == 1) {
+    //         k = true; 
+    //     }
+    //     if (k == true) {
+    //         cout << b % 2; 
+    //     }
+    // }
+    // cout << endl;
+
+
+
+// 二进制转十进制
+    long long k,sum=0,er=1;
+    cout<<"请输入一个二进制整数：";
+    cin>>k;
+    for (int i=1;k>0;i++){
+        sum+=k%10*er;
+        er*=2;
+        k=k/10;
     }
-    cout << endl;
-
-
-
+    cout<<"转换后的十进制整数为："<<sum<<endl;
 
 
 
