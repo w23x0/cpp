a9cd6d2236ecf49ab81ad5d08547f1b3c48c206d
W23x <www.zh0408@gmail.com>
2026-02-01 23:52:29 +0800

    i

--- Note/Cherno/5compiling&linking/main.cpp
+++ Note/Cherno/5compiling&linking/main.cpp
@@ -3,7 +3,23 @@ int main()
 {
     log ("hello dream");
     return 0;
-}
+
+
+// 预处理器
+// 头文件#include <>   （系统）
+// 头文件#include ""   （用户自定义）   
+
+#include "Preprocessor.h"
+
+// g++ -E *.cpp -o *.i
+// 其他预处理指令
+// #define 宏定义
+// #ifdef #ifndef 条件编译
+// #pragma 编译指令
+
+
+
 // cpps >> compiling >> object(obj) >> linking >> exe
-// g++  -o main.exe main.cpp
-// g++  -c main.cpp -o main.obj
+// g++  -o *.exe *.cpp
+// g++  -c *.cpp -o *.obj
+
---
+++ Note/Cherno/5compiling&linking/main.i
@@ -0,0 +1,17 @@
+# 1 "main.cpp"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "main.cpp"
+void log(const char*);
+int main()
+{
+    log ("hello dream");
+    return 0;
+
+
+
+
+
+# 1 "Preprocessor.h" 1
+}
+# 12 "main.cpp" 2
